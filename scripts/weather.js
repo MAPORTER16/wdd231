bdcd808a526cdf20a62919f733c981b4439e9dc9
MAPORTER16 <matthewporter2000@gmail.com>
2025-11-10 20:38:13 -0700

    weather api test done

--- scripts/weather.js
+++ scripts/weather.js
@@ -21,10 +21,15 @@ async function apifetch() {
 
 function displayResults(data) {
     currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    
     const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    
     let desc = data.weather[0].description;
+    
     weatherIcon.setAttribute('src', iconsrc);
+    
     weatherIcon.setAttribute('alt', desc);
+    
     captionDesc.textContent = `${desc}`;
 }
 
